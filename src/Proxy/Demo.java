c365ab9b1ebd9aac5eb852702d3daf4769083299
revkae <87928758+revkae@users.noreply.github.com>
2025-06-10 22:47:00 +0300

    added proxy

---
+++ src/Proxy/Demo.java
@@ -0,0 +1,17 @@
+package Proxy;
+
+public class Demo {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("test_image_1.jpg");
+        Image image2 = new ProxyImage("test_image_2.jpg");
+
+        // Image will be loaded from disk
+        image1.display();
+        
+        // Image will not be loaded from disk, it will use the cached version
+        image1.display();
+
+        // Image will be loaded from disk
+        image2.display();
+    }
+}
